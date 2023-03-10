4cde7f71dc1229a97a7258d774e72e6fc59c1434
Angel Gabriel <alquisira2004@gmail.com>
2023-03-09 18:02:24 -0600

    Se crea la clase Cliente

---
+++ src/Testcliente.java
@@ -0,0 +1,20 @@
+public class Testcliente {
+
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente("Angel Gabriel", "12345678901234567890", 12345678, 350);
+
+        cliente.toString();
+        //mostrar los datos de cuenta1
+        System.out.println("Datos del cliente");
+        System.out.println("Nombre del titular: " + cliente.getName());
+        System.out.println("Número de cuenta: " + cliente.getNoAccount());
+        System.out.println("La contraseña es: " + cliente.getPassword());
+        System.out.println("Su saldo es: " + cliente.getBalance());
+
+
+    }
+
+
+
+}
